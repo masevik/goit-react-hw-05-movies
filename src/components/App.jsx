c5ec1047e23c65aced6c0fa1b9b26e4a3d3816c0
Serhii Malychkovych <masevik@gmail.com>
2022-12-29 19:54:23 +0200

    start files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
+import { Box } from './Box';
+
 export const App = () => {
   return (
-    <div
+    <Box
       style={{
         height: '100vh',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
       React homework template
-    </div>
+    </Box>
   );
 };
--- src/components/GlobalStyle/GlobalStyle.jsx
+++ src/components/GlobalStyle/GlobalStyle.jsx
@@ -1,6 +1,8 @@
-@import-normalize; /* bring in normalize.css styles */
+import { createGlobalStyle } from 'styled-components';
+import 'modern-normalize';
 
-body {
+export const GlobalStyle = createGlobalStyle`
+  body {
   margin: 0;
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
     'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
@@ -13,3 +15,30 @@ code {
   font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
     monospace;
 }
+
+p,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  margin: 0;
+  }
+
+ul,
+ol {
+  margin: 0;
+  padding-left: 0;
+  }
+
+li {
+  list-style: none;
+  }
+
+img {
+  display: block;
+  max-width: 100%;
+  height: auto;
+  }
+`;
