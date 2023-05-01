b8420d5dccbeccad83a8c84a144de961d5819127
Serhii Malychkovych <masevik@gmail.com>
2023-05-01 14:25:53 +0300

    add strict mode

--- src/index.js
+++ src/index.js
@@ -7,11 +7,11 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-  <BrowserRouter basename="/goit-react-hw-05-movies">
-    <App />
-    <ToastContainer />
-    <GlobalStyle />
-  </BrowserRouter>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter basename="/goit-react-hw-05-movies">
+      <App />
+      <ToastContainer />
+      <GlobalStyle />
+    </BrowserRouter>
+  </React.StrictMode>
 );
